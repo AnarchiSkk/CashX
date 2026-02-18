95491392da0e0da9284e91f45540a6a8f52c43aa
AnarchiSkk <lacornetter@gmail.com>
2026-02-18 23:36:43 +0100

    server.js
    app.get('(*)', (req, res) => {
        res.sendFile(path.join(__dirname, 'dist', 'index.html'));
    });

--- server.js
+++ server.js
@@ -36,10 +36,9 @@ app.get('/api/health', (req, res) => {
 });
 
 // Catch-all route to serve index.html for SPA client-side routing
-app.get('*', (req, res) => {
+app.get('(*)', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
-
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
